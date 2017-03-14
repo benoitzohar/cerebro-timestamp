214838aeb9d847bb8d85c397c5a011233e5a4793
Benoit Zohar <benoit.zohar@gmail.com>
2017-03-13 23:01:39 -0400

    use prettier, add ability to parse timestamp without a prefix, change the icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const icon = require('./icon.png')
+const icon = require('./icon.png');
 const {
     INPUT_FORMAT,
     OUTPUT_FORMAT,
@@ -8,17 +8,22 @@ const {
     getType,
     parse,
     output
-} = require('./formatter')
+} = require('./formatter');
 
-const plugin = ({term, display, actions}) => {
-    const match = term.match(/ts\s(.*)/)
+const plugin = ({ term, display, actions }) => {
+    let match = term.match(/^(\d{10,13})/);
+
+    //fall back on the ts prefix
+    if (!match) {
+        match = term.match(/^ts\s(.*)/);
+    }
+
     if (match) {
-        const input= match[1]
-        
-        if (input) {
+        const input = match[1];
 
-            const type = getType(input) //String
-            const moment = parse(input, type) //momentJS object
+        if (input) {
+            const type = getType(input); //String
+            const moment = parse(input, type); //momentJS object
 
             if (!moment.isValid()) {
                 display({
@@ -26,14 +31,12 @@ const plugin = ({term, display, actions}) => {
                     id: 'cerebro-timestamp-error',
                     icon,
                     subtitle: ''
-                })
-            }
-            else {
-
+                });
+            } else {
                 //prepare appopriate order for rows
                 let typesInOrder;
 
-                switch(type) {
+                switch (type) {
                     case INPUT_FORMAT.TS_S:
                     case INPUT_FORMAT.TS_MS:
                         typesInOrder = [
@@ -43,7 +46,7 @@ const plugin = ({term, display, actions}) => {
                             OUTPUT_FORMAT.ISO,
                             OUTPUT_FORMAT.TIMESTAMP_MS,
                             OUTPUT_FORMAT.TIMESTAMP
-                        ]
+                        ];
                         break;
                     case INPUT_FORMAT.NOW:
                     case INPUT_FORMAT.ARRAY:
@@ -56,13 +59,12 @@ const plugin = ({term, display, actions}) => {
                             OUTPUT_FORMAT.LONG,
                             OUTPUT_FORMAT.FULL,
                             OUTPUT_FORMAT.ISO
-                        ]
+                        ];
                         break;
                 }
 
-                typesInOrder.forEach((format) => {
-
-                    const res = output(moment, format)
+                typesInOrder.forEach(format => {
+                    const res = output(moment, format);
 
                     display({
                         title: res,
@@ -71,20 +73,18 @@ const plugin = ({term, display, actions}) => {
                         clipboard: res,
                         subtitle: format.title,
                         onSelect: () => {
-                            actions.copyToClipboard(res)
+                            actions.copyToClipboard(res);
                         }
-                    })
-                })
-
-
+                    });
+                });
             }
         }
     }
 };
 
 module.exports = {
-  fn: plugin,
-  name: 'Encode/decode timestamp',
-  keyword: 'ts',
-  icon
-}
+    fn: plugin,
+    name: 'Encode/decode timestamp',
+    keyword: 'ts',
+    icon
+};
